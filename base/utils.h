1ee3cb0598f4c2531fcc399c24ea11585d438728
wt <wt1454246140@163.com>
2022-09-12 23:44:57 +0800

    Update

--- base/utils.h
+++ base/utils.h
@@ -12,9 +12,16 @@ extern "C"{
 
 #if defined(__GNUC__) || defined(__clang__)
 #define __unused __attribute__((unsed))
+#define __pure  __attribute__((pure))
+#define likely(_exp)   __builtin_expect(( _exp ), 1)
+#define unlikely(_exp) __builtin_expect(( _exp ), 0)
 #else
+
 #define __unused
-#endif
+#define __pure
+#define likely(_exp)   (_exp)
+#define unlikely(_exp) (_exp)
+#endif //defined(__GNUC__) || defined(__clang__)
 
 #ifndef CONTAINER_OF
 #define CONTAINER_OF(ptr, type, member) \
